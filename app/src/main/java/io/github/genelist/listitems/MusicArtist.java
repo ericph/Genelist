f9eb044125e16c6f2ad155489e051e12a0f49e10
Eric Parker-Howe <ericph@utexas.edu>
2018-09-16 21:28:45 -0500

    changed ListItem id from long to String, initial search

--- app/src/main/java/io/github/genelist/listitems/MusicArtist.java
+++ app/src/main/java/io/github/genelist/listitems/MusicArtist.java
@@ -1,33 +1,45 @@
 package io.github.genelist.listitems;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import de.umass.lastfm.Caller;
+import de.umass.lastfm.Artist;
+
+import io.github.genelist.util.Constants;
 import io.github.genelist.util.ImageItem;
 
 public class MusicArtist extends ListItem {
-    private long id = ListItem.DEFAULT_ID;
+    private String id = ListItem.DEFAULT_ID;
     private String name = "";
     private ImageItem image = new ImageItem();
 
     public MusicArtist() { super(); }
 
-    public MusicArtist(long id, String name, ImageItem image) {
+    public MusicArtist(String id, String name, ImageItem image) {
         exists = true;
         this.id = id;
         this.name = name;
         this.image = image;
+        Caller.getInstance().setUserAgent(Constants.LASTFM_USER);
     }
 
-    public static MusicArtist search(String query) {
-        // TODO: implement search
-        return new MusicArtist();
+    public static Collection<MusicArtist> search(String name) {
+        List<Artist> artists = new ArrayList<>(Artist.search(name, Constants.LASTFM_API_KEY));
+        return artists.stream()
+                .map(a -> new MusicArtist(a.getId(), a.getName(), new ImageItem()))
+                .collect(Collectors.toList());
     }
 
-    public static MusicArtist getById(long id) {
+    public static MusicArtist getById(String id) {
         // TODO: implement getById
         return new MusicArtist();
     }
 
     @Override
-    public long getId() { return id; }
+    public String getId() { return id; }
 
     public String getName() { return name; }
 
