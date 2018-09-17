d0c8f12f9b390e4acc8c547825c2ec7a77ae045a
Eric Parker-Howe <ericph@utexas.edu>
2018-09-17 00:40:43 -0500

    added url and fromArtist to ImageItem

--- app/src/main/java/io/github/genelist/util/ImageItem.java
+++ app/src/main/java/io/github/genelist/util/ImageItem.java
@@ -1,11 +1,26 @@
 package io.github.genelist.util;
 
+import de.umass.lastfm.Artist;
+import de.umass.lastfm.ImageSize;
+
 public class ImageItem {
     private boolean hasImage;
+    private String url = "";
 
     public ImageItem() {
         hasImage = false;
     }
 
+    public ImageItem(String url) {
+        hasImage = true;
+        this.url = url;
+    }
+
+    public static ImageItem fromArtist(Artist artist) {
+        return new ImageItem(artist.getImageURL(ImageSize.SMALL));
+    }
+
     public boolean hasImage() { return hasImage; }
+
+    public String getUrl() { return url; }
 }
