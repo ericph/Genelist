8f146b615b12fb77875a08bfd25f4b00cfe22abd
Eric Parker-Howe <ericph@utexas.edu>
2018-09-16 23:14:49 -0500

    added lastfm-java functionality to MusicArtist

--- app/src/main/java/io/github/genelist/listitems/MusicArtist.java
+++ app/src/main/java/io/github/genelist/listitems/MusicArtist.java
@@ -1,8 +1,8 @@
 package io.github.genelist.listitems;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 import de.umass.lastfm.Caller;
@@ -15,27 +15,50 @@ public class MusicArtist extends ListItem {
     private String id = ListItem.DEFAULT_ID;
     private String name = "";
     private ImageItem image = new ImageItem();
+    private Artist artist;
 
     public MusicArtist() { super(); }
 
-    public MusicArtist(String id, String name, ImageItem image) {
+    public MusicArtist(String id, String name, ImageItem image, Artist artist) {
         exists = true;
         this.id = id;
         this.name = name;
         this.image = image;
+        this.artist = artist;
         Caller.getInstance().setUserAgent(Constants.LASTFM_USER);
     }
 
-    public static Collection<MusicArtist> search(String name) {
+    public static List<MusicArtist> search(String name) {
         List<Artist> artists = new ArrayList<>(Artist.search(name, Constants.LASTFM_API_KEY));
-        return artists.stream()
-                .map(a -> new MusicArtist(a.getId(), a.getName(), new ImageItem()))
-                .collect(Collectors.toList());
+        return artists.stream().map(MusicArtist::fromArtist).collect(Collectors.toList());
     }
 
     public static MusicArtist getById(String id) {
-        // TODO: implement getById
-        return new MusicArtist();
+        try {
+            Artist a = Artist.getInfo(id, Locale.US, null, Constants.LASTFM_API_KEY);
+            return new MusicArtist(a.getMbid(), a.getName(), new ImageItem(), a);
+        } catch (Exception e) {
+            return new MusicArtist();
+        }
+    }
+
+    public static MusicArtist getByName(String name) {
+        try {
+            Artist a = Artist.getInfo(name, Locale.US, null, Constants.LASTFM_API_KEY);
+            return new MusicArtist(a.getMbid(), a.getName(), new ImageItem(), a);
+        } catch (Exception e) {
+            return new MusicArtist();
+        }
+    }
+
+    public List<MusicArtist> getSimilar() {
+        List<Artist> similar = new ArrayList<>(artist.getSimilar());
+        return similar.stream().map(MusicArtist::fromArtist).collect(Collectors.toList());
+    }
+
+    private static MusicArtist fromArtist(Artist a) {
+        return new MusicArtist(a.getMbid(), a.getName(), new ImageItem(),
+                Artist.getInfo(a.getMbid(), Locale.US, null, Constants.LASTFM_API_KEY));
     }
 
     @Override
@@ -44,4 +67,6 @@ public class MusicArtist extends ListItem {
     public String getName() { return name; }
 
     public ImageItem getImage() { return image; }
+
+    public Artist getArtist() { return artist; }
 }
