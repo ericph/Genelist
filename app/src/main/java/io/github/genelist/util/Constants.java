c7d576b4ac182f8a53eaa12c27bc0cd6b14b6395
Eric Parker-Howe <ericph@utexas.edu>
2018-09-18 22:39:15 -0500

    fixed lastfm api key

--- app/src/main/java/io/github/genelist/util/Constants.java
+++ app/src/main/java/io/github/genelist/util/Constants.java
@@ -13,6 +13,6 @@ public class Constants {
         LIST_ID_MAP.put(MusicArtist.class, "1");
     }
 
-    public static final String LASTFM_API_KEY = "b57d5651-0c90-4aae-a6fe-0ab40b57df58";
+    public static final String LASTFM_API_KEY = "c51f49fbbee8dbd1832525f55c8d3190";
     public static final String LASTFM_USER = "erq_";
 }
