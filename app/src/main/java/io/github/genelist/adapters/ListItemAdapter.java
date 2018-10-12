1d98f78c73ba48900475b513fef03d240d6113ca
Eric Parker-Howe <ericph@utexas.edu>
2018-10-12 00:34:09 -0500

    added some NonNull annotations

--- app/src/main/java/io/github/genelist/adapters/ListItemAdapter.java
+++ app/src/main/java/io/github/genelist/adapters/ListItemAdapter.java
@@ -1,5 +1,6 @@
 package io.github.genelist.adapters;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,14 +33,15 @@ public class ListItemAdapter extends RecyclerView.Adapter<ListItemAdapter.ViewHo
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    @NonNull
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.layout_list_item, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
         holder.item = gList.get(position);
         holder.rankView.setText(gList.get(position).getId());
         if (holder.item.exists && holder.item.getImage().hasImage()) {
