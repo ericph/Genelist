4eef8b79ce8d03d27ab9b5e3f98825bf1dadd4c7
Eric Parker-Howe <ericph@utexas.edu>
2018-11-12 02:11:28 -0600

    added Album and Game to LIST_ID_MAP

--- app/src/main/java/io/github/genelist/util/Constants.java
+++ app/src/main/java/io/github/genelist/util/Constants.java
@@ -3,15 +3,16 @@ package io.github.genelist.util;
 import java.util.HashMap;
 import java.util.Map;
 
-import io.github.genelist.listitems.GeneListItem;
-import io.github.genelist.listitems.Musician;
+import io.github.genelist.listitems.*;
 
 public class Constants {
     public static final Map<Class, String> LIST_ID_MAP;
     static {
         LIST_ID_MAP = new HashMap<>();
         LIST_ID_MAP.put(GeneListItem.class, "0");
-        LIST_ID_MAP.put(Musician.class, "1");
+        LIST_ID_MAP.put(Album.class, "1");
+        LIST_ID_MAP.put(Game.class, "2");
+        LIST_ID_MAP.put(Musician.class, "3");
     }
 
     public static final String LASTFM_API_KEY = "c51f49fbbee8dbd1832525f55c8d3190";
